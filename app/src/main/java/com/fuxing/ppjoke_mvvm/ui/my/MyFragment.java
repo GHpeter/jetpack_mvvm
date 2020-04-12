064ab5e803b67f477bed5e7a9a8349c88142444b
lichunfu <lcf_spark@163.com>
2020-04-12 21:49:43 +0800

    change file location

---
+++ app/src/main/java/com/fuxing/ppjoke_mvvm/ui/my/MyFragment.java
@@ -0,0 +1,38 @@
+package com.fuxing.ppjoke_mvvm.ui.my;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.fuxing.libnavannotation.FragmentDestination;
+import com.fuxing.ppjoke_mvvm.R;
+
+
+@FragmentDestination(pageUrl = "main/tabs/my", asStarter = false)
+public class MyFragment extends Fragment {
+
+    private MyViewModel myViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        myViewModel =
+                ViewModelProviders.of(this).get(MyViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        myViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
